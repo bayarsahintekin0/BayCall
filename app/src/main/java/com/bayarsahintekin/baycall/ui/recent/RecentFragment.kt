047c395e56518c51f2da23fbd2b7d4df0faf81d6
bayarsahintekin0 <bsahintekin@hotmail.com>
2022-07-23 19:15:53 +0300

    Feat:Bottom nav menu ui designed.

--- app/src/main/java/com/bayarsahintekin/baycall/ui/recent/RecentFragment.kt
+++ app/src/main/java/com/bayarsahintekin/baycall/ui/recent/RecentFragment.kt
@@ -1,4 +1,4 @@
-package com.bayarsahintekin.baycall.ui.notifications
+package com.bayarsahintekin.baycall.ui.recent
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bayarsahintekin.baycall.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class RecentFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -23,7 +23,7 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(RecentViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
--- app/src/main/java/com/bayarsahintekin/baycall/ui/recent/RecentViewModel.kt
+++ app/src/main/java/com/bayarsahintekin/baycall/ui/recent/RecentViewModel.kt
@@ -1,13 +1,13 @@
-package com.bayarsahintekin.baycall.ui.dashboard
+package com.bayarsahintekin.baycall.ui.recent
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class RecentViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is last calls Fragment"
     }
     val text: LiveData<String> = _text
 }
